303c56634c8366e9da0e4c45a3bcd780e59be691
Francisco Carneiro <69801646+ccarneirojunior@users.noreply.github.com>
2021-11-18 09:39:17 +0200

    Update manager_model.js

--- models/manager_model.js
+++ models/manager_model.js
@@ -2,24 +2,24 @@ const db = require('../database');
 
 const users = {
   get: function(callback) {
-    return db.query('select * from userName', callback); //To get from the users based on the name 
+    return db.query('select * from manager', callback); //To get from the users based on the name 
   },
   getById: function(id, callback) {
-    return db.query('select * from userName where idmanager=?', [id], callback); //To search within the users based on the ID
+    return db.query('select * from manager where idmanager=?', [id], callback); //To search within the users based on the ID
   },
   add: function(users, callback) {
     return db.query(
-      'insert into Users (idmanager,userName,password) values(?,?,?)',
+      'insert into manager (idmanager,userName,password) values(?,?,?)',
       [users.idmanager, users.userName, users.idmanager],
       callback
     );
   },
   delete: function(id, callback) {
-    return db.query('delete from Users where idmanager=?', [id], callback); //Delete based on the ID 
+    return db.query('delete from manager where idmanager=?', [id], callback); //Delete based on the ID 
   },
   update: function(id, users, callback) {        //Modification on the account information 
     return db.query(
-      'update Users set userName=?,password=?, where idUsers=?',
+      'update manager set userName=?,password=?, where idUsers=?',
       [users.idcustomer, users.userName, users.password],
       callback
     );
