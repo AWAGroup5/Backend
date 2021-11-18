d8f83554e328cac9aefd2485f38e335d6c111bc5
Francisco Carneiro <ccarneirojunior@gmail.com>
2021-11-18 13:17:56 +0200

    API

--- models/order_model.js
+++ models/order_model.js
@@ -1,15 +1,15 @@
 const db = require('../database');
 
-const orders = {
+const order = {
   get: function(callback) {
-    return db.query('select * orders', callback); //Select all orders based on ID 
+    return db.query('select * from order', callback); //Select all orders based on ID 
   },
   getById: function(id, callback) {
-    return db.query('select * from orders where idorder=?', [id], callback); //selec the order specifically based on ID 
+    return db.query('select * from order where idorder=?', [id], callback); //selec the order specifically based on ID 
   },
   add: function(orders, callback) {
     return db.query(
-      'insert into orders (idorder,idcustomer,status,cost,idrestaurant) values(?,?,?,?,?)',  //Adding a new order with needed information 
+      'insert into order (idorder,idcustomer,status,cost,idrestaurant) values(?,?,?,?,?)',  //Adding a new order with needed information 
       [orders.idorder, orders.idcustomer, orders.status, orders.cost, orders.idrestaurant],
       callback
     );
@@ -17,12 +17,12 @@ const orders = {
   delete: function(id, callback) {
     return db.query('delete from orders where idorder=?', [id], callback); //delete the order based on the ID 
   },
-  update: function(id, orders, callback) {   //updating an order based on its ID 
+  update: function(id, order, callback) {   //updating an order based on its ID 
     return db.query(
       'update orders set idrestaurant=?,idcustomer=?, status=?, cost=?, where idorder=?', 
-      [orders.idrestaurant, orders.idcustomer, orders.status, orders.cost, id],
+      [order.idrestaurant, order.idcustomer, order.status, order.cost, id],
       callback
     );
   }
 };
-module.exports = orders;
+module.exports = order;
