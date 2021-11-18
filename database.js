fb000808691b0a9b205bf0b657b73bb3f213fa2e
Francisco Carneiro <69801646+ccarneirojunior@users.noreply.github.com>
2021-11-18 12:46:09 +0200

    Update database.js

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql');
 const connection = mysql.createPool({
-  host: 'my_localhost',         //Replace with the online host integration? AWS?
-  user: 'cook',
-  password: 'cook',
-  database: 'restaurant'
+  host: 'eu-cdbr-west-01.cleardb.com',       
+  user: 'b1ea308d13f6db',
+  password: '55b0df08',
+  database: 'heroku_7bc01a9cd72ea84'
 });
 module.exports = connection;
