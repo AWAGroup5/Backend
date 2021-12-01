a1f3286d6795a034b1921a0621579421a0309675
idRisto <risto.alanen@gmail.com>
2021-12-01 13:41:44 +0200

    Orders working

--- models/order_model.js
+++ models/order_model.js
@@ -2,7 +2,7 @@ const db = require('../database');
 
 const orders = {
   get: function(callback) {
-    return db.query('select * from orders', callback); //Select all orderss based on ID 
+    return db.query('select * from orders', callback); //Select all orders based on ID 
   },
   getByCustomerId: function(id, callback) {
     return db.query('select * from orders where idcustomer=?', [id], callback); //selec the orders specifically based on customer ID 
@@ -10,19 +10,19 @@ const orders = {
   getByRestaurantId: function(id, callback) {
     return db.query('select * from orders where idrestaurant=?', [id], callback); //selec the orders specifically based on restaurant ID 
   },
-  add: function(orderss, callback) {
+  add: function(orders, callback) {
     return db.query(
-      'insert into orders (idorders,idcustomer,status,cost,idrestaurant) values(?,?,?,?,?)',  //Adding a new orders with needed information 
-      [orderss.idorders, orderss.idcustomer, orderss.status, orderss.cost, orderss.idrestaurant],
+      'insert into orders (idorder,idcustomer,status,cost,address,idrestaurant) values(?,?,?,?,?,?)',  //Adding a new orders with needed information 
+      [null, orders.idcustomer, orders.status, orders.cost, orders.address, orders.idrestaurant],
       callback
     );
   },
   delete: function(id, callback) {
-    return db.query('delete from orders where idorders=?', [id], callback); //delete the orders based on the ID 
+    return db.query('delete from orders where idorder=?', [id], callback); //delete the orders based on the ID 
   },
   update: function(id, orders, callback) {   //updating an orders based on its ID 
     return db.query(
-      'update orderss set idrestaurant=?,idcustomer=?, status=?, cost=?, where idorders=?', 
+      'update orders set idrestaurant=?,idcustomer=?, status=?, cost=? where idorder=?', 
       [orders.idrestaurant, orders.idcustomer, orders.status, orders.cost, id],
       callback
     );
--- routes/order.js
+++ routes/order.js
@@ -2,61 +2,73 @@ const express = require('express');
 const router = express.Router();
 const order = require('../models/order_model');
 
-router.get('/:id?',
- function(request, response) {
-  if (request.params.id) {
-    order.getById(request.params.id, function(err, dbResult) {
-      if (err) {
-        response.json(err);
-      } else {
-        console.log(dbResult[0]);
-        response.json(dbResult[0]);
-      }
-    });
-  } else {
-    order.get(function(err, dbResult) {
-      if (err) {
-        response.json(err);
-      } else {
-        response.json(dbResult);
-      }
-    });
-  }
-});
-
-
-router.post('/', 
-function(request, response) {
-  order.add(request.body, function(err, dbResult) {
+router.get('/restaurant/:id?',
+  function(req, res) {
+  order.getByRestaurantId(req.params.id, function(err, dbResult) {
     if (err) {
-      response.json(err);
+      res.json(err);
     } else {
       console.log(dbResult);
-      response.json(dbResult);
+      res.json(dbResult);
+    }
+  });
+});
+
+router.get('/customer/:id?',
+  function(req, res) {
+  order.getByCustomerId(req.params.id, function(err, dbResult) {
+    if (err) {
+      res.json(err);
+    } else {
+      console.log(dbResult);
+      res.json(dbResult);
+    }
+  });
+});
+
+router.get('/', function(req, res) {
+  order.get(function(err, dbResult) {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(dbResult);
+    }
+  });
+})
+
+router.post('/', 
+function(req, res) {
+  order.add(req.body, function(err, dbResult) {
+    if (err) {
+      res.json(err);
+    } else {
+      console.log(dbResult);
+      res.json(dbResult);
     }
   });
 });
 
 
 router.delete('/:id', 
-function(request, response) {
-  order.delete(request.params.id, function(err, dbResult) {
+function(req, res) {
+  order.delete(req.params.id, function(err, dbResult) {
     if (err) {
-      response.json(err);
+      res.json(err);
     } else {
-      response.json(dbResult.affectedRows);
+      res.json(dbResult.affectedRows);
     }
   });
 });
 
 
 router.put('/:id', 
-function(request, response) {
-  order.update(request.params.id, request.body, function(err, dbResult) {
+function(req, res) {
+  console.log(req.params.id)
+  order.update(req.params.id, req.body, function(err, dbResult) {
     if (err) {
-      response.json(err);
+      res.json(err);
     } else {
-      response.json(dbResult);
+      res.json(dbResult);
     }
   });
 });
